9ef1d184bdaa7af9ed729d6c21b15a941fd15b5c
SergioLSantosR <ssantosr6@miumg.edu.gt>
2025-04-28 22:54:27 -0600

    Se agregan modificaciones a la clase Vehiculo

--- src/proyecto_tres/Vehiculo.java
+++ src/proyecto_tres/Vehiculo.java
@@ -2,4 +2,12 @@ package proyecto_tres;
 
 public class Vehiculo {
     
+    private void agregarVehiculo(){}
+    
+    private void buscarVehiculo(){}
+    
+    private void eliminarVehiculo(){}
+    
+    
+    
 }
