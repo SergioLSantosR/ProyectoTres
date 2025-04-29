42fa1f81035af7efac87f18c7828909918993df1
SergioLSantosR <ssantosr6@miumg.edu.gt>
2025-04-28 22:34:20 -0600

    Creación del repositorio

---
+++ src/proyecto_tres/Nodo.java
@@ -0,0 +1,5 @@
+package proyecto_tres;
+
+public class Nodo {
+    
+}
---
+++ src/proyecto_tres/Proyecto_Tres.java
@@ -0,0 +1,9 @@
+package proyecto_tres;
+
+public class Proyecto_Tres {
+
+    public static void main(String[] args) {
+
+    }
+    
+}
