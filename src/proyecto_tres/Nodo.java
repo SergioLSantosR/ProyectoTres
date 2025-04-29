8c1289e5aec9e496cc842fe625a5aa53e05bb5e8
SergioLSantosR <ssantosr6@miumg.edu.gt>
2025-04-28 22:42:56 -0600

    Primer actualizacion

--- src/proyecto_tres/Nodo.java
+++ src/proyecto_tres/Nodo.java
@@ -1,5 +1,15 @@
 package proyecto_tres;
 
 public class Nodo {
+    Nodo siguiente;
+    Nodo anterior;
+    String dato;
+    
+    public Nodo(String dato){
+        this.dato = dato;
+        siguiente = null;
+        anterior = null;
+        
+    }
     
 }
